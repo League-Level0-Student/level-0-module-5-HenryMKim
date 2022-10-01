e1369810b2e14aa6a5f94ec0934b2fe0dd8ccc1d
league <league@192.168.1.87>
2022-09-30 18:22:48 -0700

    lll

--- src/_01_algorithms/_2_fibonacci/Fibonacci.java
+++ src/_01_algorithms/_2_fibonacci/Fibonacci.java
@@ -1,7 +1,16 @@
 package _01_algorithms._2_fibonacci;
 
 public class Fibonacci {
-	public Fibonacci() {
+	public static void main(String[] args) {
+		int number1 = 0;
+		int number2 = 1;
+		int sum = 0;
+		for(int i = 0; i < 10; i++){
+			sum = number1 + number2;
+		number1 = number2;
+		number2 = sum;
+			System.out.println(sum);
 		
-	}
+		}
+		}
 }
