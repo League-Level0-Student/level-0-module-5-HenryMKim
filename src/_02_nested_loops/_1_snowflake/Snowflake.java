5b8fa2709fc5d549e71bdc8c2a7e3323a982b351
league <league@imac-12.attlocal.net>
2022-10-07 18:27:48 -0700

    UwU

--- src/_02_nested_loops/_1_snowflake/Snowflake.java
+++ src/_02_nested_loops/_1_snowflake/Snowflake.java
@@ -1,6 +1,7 @@
 package _02_nested_loops._1_snowflake;
 
 import java.awt.Color;
+import org.jointheleague.graphical.robot.Robot;
 
 
 public class Snowflake {
@@ -8,48 +9,52 @@ public class Snowflake {
 	public static void main(String[] args) {
 
 		// #1. Make a new Robot
-
+Robot beans = new Robot();
 
 		// #2. Set your robot’s position to x=300 and y=300
-
+beans.setX(300);
+beans.setY(300);
 
 		// #3. Put the robot's pen down
-
+beans.penDown();
 
 		// #4. Set the robot’s speed to 5
-
+beans.setSpeed(5);
 
 		// #5. use a for loop below to do everything ( #6, #7, #8, #9, #12, #13, #14 ) 6 times (use i as the counter)
-
+for(int i = 0; i < 6; i++) {
 			
 			// #6. Set the pen color to black
-
+beans.setPenColor(0, 0, 0);
 			
 			// #7. move the robot by 50
-
+beans.move(50);
 			
 			// #8. turn the robot to the right by 60
-			
+			beans.turn(60);
 			
 			// #12. set the pen color to blue for even numbers of i
-
-			
+			if(i%2==0) {
+beans.setPenColor(0,0,90);
+			}
 			// #13. set the pen color to red for all odd numbers of i
-			
+			if(i%2==1) {
+				beans.setPenColor(90,0,0);
+							}
 			
 			// #9. Use a for loop to do everything (#10, #11) 6 times ( use j as a counter )
-			
+			for(int j = 0; j < 6; j++) {
 			
 					// #10. turn the robot to the left by 60
-		
+		beans.turn(-60);
 					
 					// #11. move the robot by 50 (play the start button)
-					
+					beans.move(50);
 				
-			
+			}
 			// #14. set the pen color to black
-				
-		
+			beans.setPenColor(0,0,0);
+}
 	}
 
 }
---
+++ src/_02_nested_loops/_3_for_loop_gauntlet/For_loop_gauntlet.java
@@ -0,0 +1,12 @@
+package _02_nested_loops._3_for_loop_gauntlet;
+
+public class For_loop_gauntlet {
+	public static void main(String[] args) {
+		for(int i=0; i <= 100; i++) {
+			System.out.println(i);
+		}
+		for(int o=100; o > 0; o--) {
+			System.out.println(o);
+		}
+	}
+}
