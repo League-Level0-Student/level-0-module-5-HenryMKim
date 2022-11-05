334b3e5bf19e414c3cd450929a88769a1aa7a343
league <league@imac-12.attlocal.net>
2022-11-04 18:22:25 -0700

    hi :)

--- src/_02_nested_loops/_3_for_loop_gauntlet/For_loop_gauntlet.java
+++ src/_02_nested_loops/_3_for_loop_gauntlet/For_loop_gauntlet.java
@@ -8,5 +8,46 @@ public class For_loop_gauntlet {
 		for(int o=100; o > 0; o--) {
 			System.out.println(o);
 		}
+		int p = 0;
+		for(int k = 0; k < 50; k++) {
+		
+		System.out.println(p);
+		p = p + 2;
+		}
+		int l = 1;
+		for(int y = 0; y < 50; y++) {
+		
+		System.out.println(l);
+		l = l + 2;
+		}
+		for(int h=0; h <= 500; h++) {
+			if(h%2 == 1) {
+			System.out.println(h + " Is odd.");
+			}
+			else{
+				System.out.println(h + " Is equal.");
+				}
+		}
+		int g = 0;
+		for(int t = 0; t < 112; t++) {
+		
+		System.out.println(g);
+		g = g + 7;
+		}
+		
+		int year = 2022;
+		for(int age = 12; age > 0; age--) {
+			System.out.println("In year " + year + " I was " + age + " years old.");
+
+			year = year - 1;
+		}
+		for(int b = 0; b < 3; b++) {
+			for(int d = 0; d < 3; d++) {
+				System.out.println(b + " " + d);
+			}
+			
+		}
+		}
 	}
-}
+	
+
