3e77689c94d57cfb6a3a93e91d3e8c0f8357a43c
league <league@imac-11.attlocal.net>
2023-01-20 18:31:57 -0800

    dkbhdicb kdfg hgdzbadhdhjguy bbdfv

--- src/_03_method_writing/_2_happy_pet/HappyPet.java
+++ src/_03_method_writing/_2_happy_pet/HappyPet.java
@@ -5,23 +5,27 @@ public class HappyPet {
 	
 	// 1. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
-	
+	static int happinessLevel = 0;
 	public static void main(String[] args) {
+		if(happinessLevel < 5) {
 		// 2. Ask the user what kind of pet they want to buy, and store their answer in a variable
-
+String pet = JOptionPane.showInputDialog(null, "Which pet do you want to buy? ");
 		// 3. REPEAT steps 4 - 7 enough times to make your pet happy!
 		
 			// 4. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
+			int task = JOptionPane.showOptionDialog(null, "What do you want to do to make your pet " +  pet + " happy?", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "Culddle with your " + pet + ".", "Walk your " + pet + ".", "Feed your " + pet + ".", "Clean up your " + pet + "'s poop."}, null);
 
 			// 6. Use user input to call the appropriate method created in step 5 below.
-
-			// 7. If you determine the happiness level is large enough, tell the
+if(task = 1) {
+	happinessLevel = happinessLevel + 1;
+}
+	// 7. If you determine the happiness level is large enough, tell the
+		
 			//    user that he loves his pet and use break; to exit for loop.
-
+		}
 	}
 
 	// 5. Create methods to handle each of your user selections.
